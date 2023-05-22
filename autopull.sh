c768c3791af2d37a8c34b28f8fcd63fa0bdabc59
laramerdol <laramerdol@gmail.com>
2023-05-22 15:47:43 +0300

    Change token

--- autopull.sh
+++ autopull.sh
@@ -4,5 +4,5 @@ spawn ./gitpull.sh
 expect -exact "Username for 'https://github.com': "
 send -- "laramerdol\r"
 expect -exact "Password for 'https://laramerdol@github.com':"
-send -- "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\r"
+send -- "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\r"
 expect eof
