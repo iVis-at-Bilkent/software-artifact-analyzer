aa7bdeca13251c458d712591902c0616544eb5b0
laramerdol <laramerdol@gmail.com>
2023-10-28 18:58:41 +0300

    Neo4j update for the software artifact analyzer

--- autopull.sh
+++ autopull.sh
@@ -4,5 +4,5 @@ spawn ./gitpull.sh
 expect -exact "Username for 'https://github.com': "
 send -- "laramerdol\r"
 expect -exact "Password for 'https://laramerdol@github.com':"
-send -- "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\r"
+send -- "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\r"
 expect eof
