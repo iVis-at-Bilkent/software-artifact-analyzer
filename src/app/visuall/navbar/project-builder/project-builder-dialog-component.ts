110c9ee3d34515fd8e18bf2c06eee3563eefd725
laramerdol <laramerdol@gmail.com>
2023-12-30 15:26:11 +0300

    Fix issue #51 : Add Statistics part

--- src/app/visuall/navbar/project-builder/project-builder-dialog-component.ts
+++ src/app/visuall/navbar/project-builder/project-builder-dialog-component.ts
@@ -1,9 +1,11 @@
-import { Component, NgModule, ViewChild } from '@angular/core';
+import { Component, Injector, NgModule, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { DialogComponent } from './dialog-component';
-import {ProjectAboutModalComponent} from '../../popups/project-about-modal/project-about-modal-component';
+import { ProjectAboutModalComponent } from '../../popups/project-about-modal/project-about-modal-component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Neo4jDb } from '../../db-service/neo4j-db.service';
+import { DbResponseType, GraphResponse } from 'src/app/visuall/db-service/data-types';
 /**
  * @title Dialog elements
  */
@@ -13,7 +15,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: 'project-builder-dialog-component.html',
 })
 export class ProjectBuilderDialogComponent {
-  constructor(public dialog: MatDialog,  private _modalService: NgbModal) {}
+  constructor(public dialog: MatDialog, private _modalService: NgbModal, public _dbService: Neo4jDb) { }
 
   openNewDialog() {
     const dialogRef = this.dialog.open(DialogComponent, {
@@ -26,9 +28,48 @@ export class ProjectBuilderDialogComponent {
     });
   }
 
-  openAboutDialog(){
-    this._modalService.open(ProjectAboutModalComponent);
+  openAboutDialog() {
+    // Initialize the statistic object
+    const statistic: any = {};
+
+    const cb = (x) => {
+      // Use square brackets to access properties
+      for (let j = 0; j < x.columns.length; j++) {
+        statistic[x.columns[j]] = x.data[0][j];
+      }
+
+      // Pass the configuration object as a 'data' property
+      const modalRef = this._modalService.open(ProjectAboutModalComponent);
+
+      // Access the component instance and set its properties
+      const modalComponentInstance: ProjectAboutModalComponent = modalRef.componentInstance;
+      modalComponentInstance.modalConfig = statistic;
+    };
+
+    const cql = `
+      MATCH (n)
+      OPTIONAL MATCH (n)-[r]->()
+      WITH
+        COUNT(DISTINCT n) AS node,
+        COUNT(DISTINCT r) AS edge,
+        COLLECT(DISTINCT CASE WHEN 'Commit' IN labels(n) THEN n END) AS commitNodes,
+        COLLECT(DISTINCT CASE WHEN 'PullRequest' IN labels(n) THEN n END) AS prNodes,
+        COLLECT(DISTINCT CASE WHEN 'Issue' IN labels(n) THEN n END) AS issueNodes,
+        COLLECT(DISTINCT CASE WHEN 'Developer' IN labels(n) THEN n END) AS developerNodes,
+        COLLECT(DISTINCT CASE WHEN 'File' IN labels(n) THEN n END) AS fileNodes
+      RETURN
+        node,
+        edge,
+        SIZE(commitNodes) AS commitNode,
+        SIZE(prNodes) AS prNode,
+        SIZE(issueNodes) AS issueNode,
+        SIZE(developerNodes) AS developerNode,
+        SIZE(fileNodes) AS fileNode;
+      `;
+
+    this._dbService.runQuery(cql, cb, DbResponseType.table);
   }
 }
+
 
 
--- src/app/visuall/popups/project-about-modal/project-about-modal-component.ts
+++ src/app/visuall/popups/project-about-modal/project-about-modal-component.ts
@@ -1,30 +1,37 @@
-import { Component, OnInit, ViewChild, AfterViewChecked, ElementRef, OnDestroy } from '@angular/core';
+import { Component,Input, OnInit, ViewChild, AfterViewChecked, ElementRef, OnDestroy } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Neo4jDb } from '../../db-service/neo4j-db.service';
+import { DbResponseType, GraphResponse } from 'src/app/visuall/db-service/data-types';
+
 @Component({
+  
   selector: 'project-about-modal-component',
   templateUrl: './project-about-modal-component.html',
   styleUrls: ['./project-about-modal-component.css']
 })
+
 export class ProjectAboutModalComponent implements OnInit, AfterViewChecked, OnDestroy {
+  @Input() modalConfig: any;
   @ViewChild('closeBtn', { static: false }) closeBtnRef: ElementRef;
   projectName: string;
   githubUrl: string;
   jiraUrl: string;
+  statistic:any;
 
-  constructor(public activeModal: NgbActiveModal, private http: HttpClient,) { }
+
+  constructor(public activeModal: NgbActiveModal, private http: HttpClient) { }
 
   ngOnInit() {
+    this.statistic =this.modalConfig;
     this.http.get(`http://${window.location.hostname}:4445/getAuthentication`).subscribe(data => {
-    if (data) {
-      this.projectName = data["github_repo"];
-      this.githubUrl = "https://github.com/"+data["github_repo"];
-      this.jiraUrl = data["jira_url"]+"/projects/"+data["jira_key"];
-  }
+      if (data) {
+        this.projectName = data["github_repo"];
+        this.githubUrl = "https://github.com/" + data["github_repo"];
+        this.jiraUrl = data["jira_url"] + "/projects/" + data["jira_key"];
+      }
     });
-
   }
-
   ngAfterViewChecked() {
     this.closeBtnRef.nativeElement.blur();
   }
