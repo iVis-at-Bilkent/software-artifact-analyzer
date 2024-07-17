3cd8ffe98901580c37d391d47381c4be9cd5e9c4
laramerdol <laramerdol@gmail.com>
2024-07-17 23:21:19 +0300

    Chnage authentication

--- autopull.sh
+++ autopull.sh
@@ -2,7 +2,7 @@
 # execute git pull and then enter username, personel access token using 'expect'
 spawn ./gitpull.sh
 expect -exact "Username for 'https://github.com': "
-send -- "laramerdol\r"
-expect -exact "Password for 'https://laramerdol@github.com':"
-send -- "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\r"
+send -- "saanalyzer\r"
+expect -exact "Password for 'https://saanalyzer@github.com':"
+send -- "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\r"
 expect eof
