84b903a8ba15f54b9eba6480aff2480622485d94
laramerdol <laramerdol@gmail.com>
2024-09-12 20:54:19 +0300

    Fix issue #158:Simplify the root folder

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 PATH=/usr/bin:/bin:/home/ivis/.nvm/versions/node/v14.19.3/bin
 echo "starting runner $(date)" >> bash-script.log
-npm i >> bash-script.log
-npm run ng build >> bash-script.log
-sudo "$(which node)" server.js >> bash-script.log
-echo "runner ended $(date)" >> bash-script.log
+cd ..
+npm i 
+npm run ng build 
+sudo "$(which node)" server.js 
+echo "runner ended $(date)" 
