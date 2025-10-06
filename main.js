e5cb4a1953e3b6b5062ebf0aa427821a1041defa
Lara Merdol <59064089+LaraMerdol@users.noreply.github.com>
2025-10-06 12:44:01 +0200

    Delete main.js

--- main.js
+++
@@ -1,3 +0,0 @@
-document.write("    <footer id=\"content\">");
-document.write("      &copy; Copyright 2024 <a href=\"http://www.cs.bilkent.edu.tr/~ivis/\">i-Vis</a> \& <a href=\"http://bilsen.cs.bilkent.edu.tr\">BILSEN</a> at Bilkent University");
-document.write("    </footer>");
